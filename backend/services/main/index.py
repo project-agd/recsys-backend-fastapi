5c34b0acc578a83a64b3b30f1635511b12509879
Juneseok Byun <byunjuneseok@gmail.com>
2021-05-20 16:36:16 +0900

    Init content based filtering

--- backend/services/main/index.py
+++
@@ -1,9 +0,0 @@
-from backend import _get_session
-from backend.db.models.store import Store
-
-
-async def index():
-    session_local = _get_session()
-    return {
-        "result": session_local.query(Store).all()
-    }
