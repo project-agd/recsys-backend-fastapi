af47b3b7cb1583b9770a49cd4842dece7fedddaf
Juneseok Byun <byunjuneseok@gmail.com>
2021-05-23 15:31:04 +0900

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 import json
 import typing
-from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import Response
 
 from .services import store, collaborative_filtering, content_based_filtering
@@ -22,6 +22,19 @@ class PrettyJSONResponse(Response):
 
 
 app = FastAPI()
+
+origins = [
+    'http://localhost',
+    'http://localhost:3000'
+   ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/v0/store/{store_id}', response_class=PrettyJSONResponse)
