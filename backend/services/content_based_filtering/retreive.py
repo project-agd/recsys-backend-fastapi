58c0a4853ac98030e03780c2972630324c6039b8
Juneseok Byun <byunjuneseok@gmail.com>
2021-05-24 14:05:05 +0900

    Content based filtering Completed

--- backend/services/content_based_filtering/retreive.py
+++ backend/services/content_based_filtering/retreive.py
@@ -3,17 +3,17 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 from backend import _get_session
-from backend.db.models.store import StoreStoreCategoryMap
+from backend.db.models.store import StoreStoreCategoryMap, Store
 
 
 def retrieve_by_store(store_id: str):
     session_local = _get_session()
 
-    all_categories_map = session_local.query(StoreStoreCategoryMap).order_by(StoreStoreCategoryMap.store_id).all()
+    all_category_maps = session_local.query(StoreStoreCategoryMap).order_by(StoreStoreCategoryMap.store_id).all()
 
     data = list()
 
-    for mm in all_categories_map:
+    for mm in all_category_maps:
         if len(data) == 0:
             data.append({
                 'store_id': mm.store_id,
@@ -38,21 +38,27 @@ def retrieve_by_store(store_id: str):
 
     count_vector = CountVectorizer(ngram_range=(1, 3))
     count_vector_categories = count_vector.fit_transform(data['store_category_ids'])
-
     category_cosine_similarity = cosine_similarity(count_vector_categories, count_vector_categories).argsort()[:, ::-1]
     target_store_index = data[data['store_id'] == store_id].index.values
 
     similar_indexes = category_cosine_similarity[target_store_index, :5].reshape(-1)
     similar_indexes = similar_indexes[similar_indexes != target_store_index]
 
-    similar_categories = list()
+    similar_stores_indexes = list()
     for ss in similar_indexes:
-        similar_categories.append(int(ss))
+        similar_stores_indexes.append(int(ss))
+
+    similar_stores = list()
+    for ss in similar_stores_indexes:
+        dd = data.iloc[ss]
+        store = session_local.query(Store).filter(Store.id == dd['store_id']).first()
+        dd['details'] = store
+        similar_stores.append(dd)
 
     return {
         'result': {
-            'store_id': store_id,
+            'requested_store':  session_local.query(Store).filter(Store.id == store_id).first(),
             'algorithm': 'cb',
-            'recommendations': similar_categories
+            'recommendations': similar_stores
         }
     }
